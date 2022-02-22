984a82cede3ef3aaef988bfd4845b5e64fad6dcc
fikri-dev <fan10062003@gmail.com>
2022-02-22 09:46:15 +0700

    adding some changes to single post page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.main', ["title" => "Fiblo | Blog"])
 
 @section('content')
-<h2>{{ $post->title }}</h2>
-<p class="mb-3">
-    Wrote by <a href="/posts/author/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a
-        href="/categories/{{ $post->category->slug }}">{{
-        $post->category->name }}</a>
-</p>
-{!! $post->body !!}
+<div class="container">
+    <div class="row justify-content-center mb-5">
+        <div class="col-md-7">
+            <img src="/images/hero.jpg" class="card-img-top w-100 h-100 hero-image position-relative img-fluid" style="left: 0; max-height: 400px">
+            <h2>{{ $post->title }}</h2>
+            <p class="mb-3">
+                Wrote by
+                <a class="author" href="/posts/author/{{ $post->author->username }}">{{ $post->author->name }}</a> in
+                <a class="category" href="/posts/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            </p>
+            <article class="fs-5">{!! $post->body !!}</article>
 
-<br>
-<a href="/posts" class="btn btn-danger btn-sm text-white mt-3">Back to posts</a>
+            <a href="/posts" class="btn btn-danger btn-sm text-white mt-3 d-inline-block">Back to posts</a>
+        </div>
+    </div>
+</div>
+
 @endsection
