fc0df834803d2662671ba47b0cb1bc38be12621f
fikri-dev <fan10062003@gmail.com>
2022-02-23 13:15:27 +0700

    change to php arrow function in Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,25 +26,34 @@ class Post extends Model
     public function scopeFilter($resource, array $filters)
     {
         // kembalikan resource(data post) yang title dan bodynya sesuai request('search') jika ada request('search') / user sedang melakukan searching 
-        $resource->when($filters['search'] ?? false, function ($resource, $search) {
-            return $resource->where('title', 'like', "%$search%")
-                ->orWhere('body', 'like', "%$search%");
-        });
+        $resource->when(
+            $filters['search'] ?? false,
+            fn ($resource, $search) =>
+            $resource->where('title', 'like', "%$search%")
+                ->orWhere('body', 'like', "%$search%")
+        );
 
-        $resource->when($filters['category'] ?? false, function ($resource, $category) {
+        $resource->when(
+            $filters['category'] ?? false,
+            fn ($resource, $category) =>
             // kembalikan resource(data post) yang mempunyai category yang field slug di table categorynya = request('category')
             // kode dibawah mirip seperti $post->category->slug = request('category')
-            return $resource->whereHas('category', function ($resource) use ($category) {
-                $resource->where('slug', $category);
-            });
-        });
+            $resource->whereHas(
+                'category',
+                fn ($resource) => ($resource->where('slug', $category))
+            )
+        );
 
-        $resource->when($filters['author'] ?? false, function ($resource, $author) {
+        $resource->when(
+            $filters['author'] ?? false,
+            fn ($resource, $author) =>
             // kembalikan resource(data post) yang mempunyai author yang field username di table usernya = request('author')
             // kode dibawah mirip seperti $post->author->username = request('author')
-            return $resource->whereHas('author', function ($resource) use ($author) {
-                dd($resource->where('username', $author));
-            });
-        });
+            $resource->whereHas(
+                'author',
+                fn ($resource) =>
+                $resource->where('username', $author)
+            )
+        );
     }
 }
