365611635b06dadc38ec41c943b9a581def3ea36
fikri-dev <fan10062003@gmail.com>
2022-02-26 09:22:52 +0700

    adding dashboard view file and feather icons

---
+++ resources/views/dashboard/layouts/main.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fiblo | Dashboard</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/dashboard.css" rel="stylesheet">
+
+    {{-- CSS feather icons --}}
+    <link rel="stylesheet" href="/css/feathericon.min.css">
+</head>
+
+<body>
+
+    @include('dashboard.partials.header')
+
+    <div class="container-fluid">
+        <div class="row">
+            @include('dashboard.partials.nav')
+
+            @yield('content')
+
+        </div>
+    </div>
+
+    <script src="/js/bootstrap.bundle.min.js"></script>
+
+    <script src="/js/feather.min.js"></script>
+
+    <script src="/js/dashboard.js"></script>
+</body>
+
+</html>
---
+++ resources/views/dashboard/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Fiblo</a>
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu"
+        aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    <div class="navbar-nav">
+        <div class="nav-item text-nowrap">
+            <form action="/logout" method="post">
+                @csrf
+                <button class="nav-link px-3 border-0 bg-dark" type="submit">
+                    <i data-feather="log-out"></i> Logout
+                </button>
+            </form>
+        </div>
+    </div>
+</header>
