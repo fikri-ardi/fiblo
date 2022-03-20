c00de113f717bf9e70ead3da77009002ef85543d
fikri-dev <fan10062003@gmail.com>
2022-03-20 10:40:09 +0700

    replace breeze app.blade.php with the old app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,49 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<!doctype html>
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link rel="shortcut icon" href="/icon/fiblo.png" type="image/x-icon">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- Tailwind CSS --}}
+    <link rel="stylesheet" href="/css/app.css">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/style.css">
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
+    {{-- Bootstrap icons --}}
+    <link rel="stylesheet" href="/font/bootstrap-icons.css">
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+    <script src="/js/app.js" defer></script>
+    <script src="/js/bootstrap.bundle.min.js"></script>
+
+    {{-- Google Fonts --}}
+    {{--
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> --}}
+    {{--
+    <link
+        href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap"
+        rel="stylesheet"> --}}
+
+    <title>{{ env('APP_NAME') ." | ". $title ?? '' }}</title>
+    {{ $style }}
+</head>
+
+<body class="antialiased">
+    <x-navbar></x-navbar>
+    <x-_notif></x-_notif>
+
+    <div class="container main-container mt-2 mb-5 flex flex-col pt-28 min-h-screen">
+        {{ $slot }}
+    </div>
+
+    <x-_footer></x-_footer>
+    {{ $script ?? null }}
+</body>
+
 </html>
