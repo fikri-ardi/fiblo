9c36751014eae1315935b4c41aa5ba7802c09db1
fikri-dev <fan10062003@gmail.com>
2022-03-23 23:19:35 +0700

    add default string lenght setting in AppServiceProvider boot method for old mysql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use App\Http\Middleware\CacheResponse;
 use Illuminate\Pagination\Paginator;
+use App\Http\Middleware\CacheResponse;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
