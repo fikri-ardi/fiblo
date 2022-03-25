a0b9497aa2862ad218216224b0e43607bbc7793b
fikri-dev <fan10062003@gmail.com>
2022-03-25 18:01:31 +0700

    make a dinamic create & edit post form

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -5,60 +5,7 @@
         </div>
 
         <div class="col-lg-8 mb-5">
-            <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="mb-3">
-                    <div class="relative w-full h-48 sm:h-96 bg-slate-300 rounded-xl flex items-center justify-center overflow-hidden">
-                        <img class="img-preview img-fluid rounded-lg w-full h-full object-cover object-center absolute">
-                        <i class="bi bi-camera-fill font-bold text-4xl text-slate-500"></i>
-                        <input class="absolute h-full w-full opacity-0 form-control img-input @error('image') is-invalid @enderror" type="file"
-                            id="image" name="image" onchange="previewImage()">
-                        <div
-                            class="absolute bottom-0 left-0 right-0 h-10 bg-black bg-opacity-50 flex items-center justify-center font-semibold text-white backdrop-blur-xl pointer-events-none">
-                            Ubah
-                        </div>
-                    </div>
-                    @error('image')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="mb-3">
-                    <input type="text" id="title" name="title" value="{{ old('title', isset($post) ? $post->title : '') }}" placeholder="Judul"
-                        class="text-2xl sm:text-3xl border-0 w-full focus:ring-0" autofocus>
-                </div>
-
-                <div class="mb-3" style="min-height: 500px;">
-                    @error('body')
-                    <p class="text-danger">{{ $message }}</p>
-                    @enderror
-                    <input type="hidden" name="body" id="body" value="{{ old('body') }}" required>
-                    <trix-editor input="body" placeholder="Tulis cerita kamu..." class="text-lg border-0"></trix-editor>
-                </div>
-
-                <input type="hidden" id="slug" name="slug" value="{{ old('slug', isset($post) ? $post->slug : '') }}">
-
-                <div class="mb-3">
-                    <select class="form-select @error('category_id') is-invalid @enderror" name="category_id">
-                        <option selected disabled>Pilih Category</option>
-                        @forelse ($categories as $category)
-                        <option value="{{ $category->id }}" {{ $category->id == old('category_id') ? 'selected' : '' }}>{{ $category->name }}</option>
-                        @empty
-                        <option disabled>Maaf, kamu belum punya category :v</option>
-                        @endforelse
-                    </select>
-                    @error('category_id')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="flex justify-end">
-                    <x-_button>
-                        <span class="bi bi-upload"></span>
-                        Buat Post
-                    </x-_button>
-                </div>
-            </form>
+            <x-posts._form :action="route('posts.store')" :categories="$categories" :post="$post"></x-posts._form>
         </div>
     </main>
 
--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -4,57 +4,9 @@
             <h1 class="h2">Ubah Post</h1>
         </div>
 
-        <div class="col-lg-8">
-            <form action="{{ route('posts.update', $post) }}" method="POST" enctype="multipart/form-data" class="mb-4">
-                @method('put')
-                @csrf
-                <x-_input name="title" :model="$post" label="Title"></x-_input>
-                <x-_input name="slug" :model="$post" label="Slug"></x-_input>
-
-                <div class="mb-3">
-                    <label for="category" class="form-label">Category</label>
-                    <select class="form-select @error('category_id') is-invalid @enderror" name="category_id">
-                        <option selected disabled>Pilih Category</option>
-                        @forelse ($categories as $category)
-                        <option value="{{ $category->id }}" {{ $category->id == old('category_id', $post->category_id) ? 'selected' : '' }}>{{
-                            $category->name }}
-                        </option>
-                        @empty
-                        <option disabled>Maaf, kamu belum punya category :v</option>
-                        @endforelse
-                    </select>
-                    @error('category_id')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="mb-3">
-                    <label for="image" class="form-label">Banner</label>
-                    <img src='{{ $post->image ? "/storage/$post->image" : "" }}'
-                        class="img-preview img-fluid col-sm-5 rounded-lg{{ $post->image ? ' mb-3' : '' }}">
-                    <input class="form-control img-input @error('image') is-invalid @enderror" type="file" id="image" name="image"
-                        onchange="previewImage()">
-                    @error('image')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="mb-3">
-                    <label for="category" class="form-label">Body</label>
-                    @error('body')
-                    <p class="text-danger">{{ $message }}</p>
-                    @enderror
-                    <input type="hidden" name="body" id="body" value="{{ old('body', $post->body) }}" required>
-                    <trix-editor input="body"></trix-editor>
-                </div>
-
-                <div class="flex justify-end">
-                    <x-_button>
-                        <span class="bi bi-pencil"></span>
-                        Ubah Post
-                    </x-_button>
-                </div>
-            </form>
+        <div class="col-lg-8 mb-5">
+            <x-posts._form :action="route('posts.update', $post)" :categories="$categories" :post="$post" method="put" button="Ubah">
+            </x-posts._form>
         </div>
     </main>
 
