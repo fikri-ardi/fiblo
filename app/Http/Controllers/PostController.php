20b93821dd27b3a228036f72613c5f749244189e
fikri-dev <fan10062003@gmail.com>
2022-03-28 13:56:25 +0700

    update the way to except auth middleware for index and show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index', 'show']]);
+        $this->middleware('auth')->except('index', 'show');
     }
 
     public function index()
